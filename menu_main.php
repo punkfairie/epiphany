5372d06da56f8d6f4d41749b829ad6a5e7e56928
Marley <marleyrae1997@gmail.com>
2022-11-15 19:06:41 -0800

    fix missing newline in nav

--- menu_main.php
+++ menu_main.php
@@ -21,7 +21,7 @@
 
 <div class="nav">
     <a href="somethinfunny/index.php" target="_top">crew site</a><br>
-    <a href="https://saudade.punkfairie.net/" target="_blank">archive site</a>
+    <a href="https://saudade.punkfairie.net/" target="_blank">archive site</a><br>
     <a href="https://punkfairie.net/" target="_blank">domain</a>
 </div>
 
